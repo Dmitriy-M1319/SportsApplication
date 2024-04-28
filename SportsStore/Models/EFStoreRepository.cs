9fbb6fafd6e437d5c90610382b3c29889df7d714
Dmitriy Mamonov <dimon.5642014@yandex.ru>
2024-04-28 18:54:50 +0300

    create admin page with Blazor components

--- SportsStore/Models/EFStoreRepository.cs
+++ SportsStore/Models/EFStoreRepository.cs
@@ -9,4 +9,20 @@ public class EFStoreRepository: IStoreRepository
     }
 
     public IQueryable<Product> Products => context.Products;
+    public void SaveProduct(Product p)
+    {
+        context.SaveChanges();
+    }
+
+    public void CreateProduct(Product p)
+    {
+        context.Add(p);
+        context.SaveChanges();
+    }
+
+    public void DeleteProduct(Product p)
+    {
+        context.Remove(p);
+        context.SaveChanges();
+    }
 }
--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,16 +1,24 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SportsStore.Models;
 
 public class Product
 {
     public long? ProductID { get; set; }
+    
+    [Required(ErrorMessage = "Please enter a name")]
     public string Name { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Please enter a description")]
     public string Description { get; set; } = string.Empty;
 
+    [Required]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
     [Column(TypeName = "decimal (8,2)")]
     public decimal Price { get; set; }
 
+    [Required(ErrorMessage = "Please specify a category")]
     public string Category { get; set; } = string.Empty;
 
 
